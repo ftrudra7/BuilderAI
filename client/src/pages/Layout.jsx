e3f36e08ed25a25d25433c69266b58a8d1be907e
ftrudra7 <rudrajha800@gmail.com>
2026-08-05 21:56:26 +0530

    Add Layout component and AppContext for state management

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
+    <>
     <div>
-      
+      Layout
     </div>
+    <Outlet />
+    </>
   )
 }
 
